3fac65f989f24404a73a7ee85779517aa47c6a05
Zaid <nolosian@gmail.com>
2022-11-13 16:37:24 -0500

    first_version - lock based queue DLL

---
+++ BasicMessagePassing.cpp
@@ -0,0 +1,209 @@
+#include "BasicMessagePassing.h"
+
+// init all data members
+BasicMessagePassing::BasicMessagePassing() {
+	m_msgs.lock();
+	created_msgs_head = NULL;
+	created_msgs_tail = NULL;
+	m_msgs.unlock();
+
+	for (int i = 0; i < MAX_THREADS_POSSIBLE; i++) {
+		queues_head[i] = queues_tail[i] = NULL;
+	}
+}
+// delete all items in all doubly linked lists
+BasicMessagePassing::~BasicMessagePassing() {
+
+	message_t *nxt_msg;
+	m_msgs.lock();
+	message_t *at_msg= created_msgs_head;
+	while (at_msg != created_msgs_tail) {
+		nxt_msg = at_msg->next;
+		delete at_msg;
+		at_msg = nxt_msg;
+	}
+	m_msgs.unlock();
+
+	message_wrapper* at_wrapper;
+	message_wrapper* nxt_wrapper;
+	for (int i = 0; i < MAX_THREADS_POSSIBLE; i++){
+		m_queue[i].lock();
+		at_wrapper = queues_head[i];
+		while (at_wrapper != queues_tail[i]) {
+			nxt_wrapper = at_wrapper->next;
+			delete at_wrapper;
+			at_wrapper = nxt_wrapper;
+		}
+		m_queue[i].unlock();
+	}
+}
+
+/*
+  Steps to creating a new message:
+  1 - Create a new message struct object
+      - if bad_alloc error is caught, print err message and return NULL
+  2 - intialize the data in the newly created message: 
+      - len <= 0
+	  - data[MAX_DATA_LENTH] <= 00..
+	  - prev & next pointers <= NULL
+  3 - add it to the created_msgs_doubly_linked_list -- will be used to keep track of all created messsages for the destructor to clear them
+  4 - return the new message object address in memory
+ */
+message_t* BasicMessagePassing::new_message() {
+
+	message_t* new_msg;
+	try {
+		new_msg = new message_t;
+	}
+	catch (const std::bad_alloc& e) {
+		// mem alloc failed, return NULL
+		std::cout << "!!ERR!! error while creating message\n";
+		std::cout << e.what() << std::endl;
+		return NULL;
+	}
+	catch (...) {
+		std::cout << "!!!ERR!!! other error while creating a message" << std::endl;
+		return NULL;
+	}
+	// 2 - clear new message content
+	new_msg->len = 0; // legal length is 0 - MAX_DATA_LENTH. 
+	for (int i = 0; i < MAX_DATA_LENTH; i++) {
+		new_msg->data[i] = 0x00;		// init the data with repeated value
+	}
+	new_msg->next = NULL;
+	new_msg->prev = NULL;
+
+	// 3 - add it to the linked list
+	m_msgs.lock();
+	if (created_msgs_head == NULL) {
+		created_msgs_head = created_msgs_tail = new_msg;		// add to empty Linked List 
+	}
+	else {
+		created_msgs_tail->next = new_msg;		// add to tail of Linked List
+		new_msg->prev = created_msgs_tail;
+		created_msgs_tail = new_msg;
+	}
+	m_msgs.unlock();
+	return new_msg;
+
+}
+/*
+  Steps to deleting a message:
+   - Find all wrapper messages in the MAX_THREADS_POSSIBLE queues pointing to msg and delete the wrappers
+   - Find it in the doubly linked list of created message objects
+*/
+// before deleting the message, search for all the wrapper pointing to it and delete them first 
+void BasicMessagePassing::delete_message(message_t* msg) {
+	if (msg == NULL) {
+		std::cout << "!!ERR!! BasicMessagePassing::delete_message received invalid msg address == NULL\n";
+		return;
+	}
+
+	// Search for the msg in all queues, delete it from all linked lists, 
+	// then delete the object
+	message_t* at_msg; 
+
+	m_msgs.lock();
+	at_msg = created_msgs_head;
+	while (at_msg != created_msgs_tail) {
+		if ( at_msg == msg ) {
+			if (at_msg->prev != NULL) {	// something behind us? then update it
+				at_msg->prev->next = at_msg->next;
+			}
+			else {						// else we were the head!
+				created_msgs_head = at_msg->next;
+			}
+			if (at_msg->next != NULL) { // something ahead? then update it
+				at_msg->next->prev = at_msg->prev;
+			}
+			else {						// else we were tail!
+				created_msgs_tail = at_msg->prev;
+			}
+		}
+		at_msg = at_msg->next;
+	}
+	m_msgs.unlock();
+
+	try {
+		delete msg;
+	}
+	catch (...) {
+		std::cout << "!!ERR!! error while deleting a message at address: " << (void*)msg << std::endl;
+	}
+}
+
+// - Validate inputs
+//			destination_id : valid range [0 - MAX_THREADS_POSSIBLE -1]
+// 1 - Creates a new message wrapper in the queue for thread_id,
+//     init new wrapper dst and msg
+// 2 - Update the corresponding doubly linked list 
+
+// return error code or zero if success
+int BasicMessagePassing::send(uint8_t destination_id, message_t* msg) {
+	if (destination_id < 0 || destination_id >= MAX_THREADS_POSSIBLE) {
+		std::cout << "!!ERR!! BasicMessagePassing::send received invalid destination id value: " << (int)destination_id << '\n';
+		std::cout << "        valid values of destination id : [0 - " << MAX_THREADS_POSSIBLE << " - 1]\n";
+		return 0;		// TODO - return an error code
+	}
+
+	if (msg == NULL) {
+		std::cout << "!!ERR!! BasicMessagePassing::send received invalid msg address == NULL\n";
+		return 0;		// TODO - return an error code
+	}
+
+	int ret = 0; 
+	message_wrapper* new_wrapper;
+	try {
+		new_wrapper = new message_wrapper;
+	}
+	catch (const std::bad_alloc& e) {
+		std::cout << "!!ERR!! BasicMessagePassing::send while creating a wrapper for a message\n";
+		std::cout << e.what() << std::endl;
+		return 0;		// TODO - error code
+	}
+	new_wrapper->dst = destination_id;
+	new_wrapper->msg = msg;
+
+	m_queue[destination_id].lock();
+	if (queues_head[destination_id] == NULL) { // first message in an empty queue
+		queues_head[destination_id] = queues_tail[destination_id] = new_wrapper;  // add to empty linked list 
+	}
+	else {			// add to tail of queue
+		queues_tail[destination_id]->next = new_wrapper;
+		new_wrapper->prev = queues_tail[destination_id];
+		queues_tail[destination_id] = new_wrapper;
+	}
+	m_queue[destination_id].unlock();
+
+	return ret;
+}
+
+// consume a message wrapper object from the head of the receiver_id queue
+// receiving a message doesn't delete the message it just deletes a wrapper that points to it
+// return error code, or 0 if success.
+int BasicMessagePassing::recv(uint8_t receiver_id, message_t* msg) {
+	if (receiver_id < 0 || receiver_id >= MAX_THREADS_POSSIBLE) {
+		std::cout << "!!ERR!! BasicMessagePassing::recv received invalid receiver_id id value: " << (int)receiver_id << '\n';
+		std::cout << "        valid values of destination id : [0 - " << MAX_THREADS_POSSIBLE << " - 1]\n";
+		return 0;		// TODO - return an error code
+	}
+	if (msg == NULL) {
+		std::cout << "!!ERR!! BasicMessagePassing::recv received invalid msg address == NULL\n";
+		return 0;		// TODO - return an error code
+	}
+
+	m_queue[receiver_id].lock();
+	if (queues_head[receiver_id] == NULL) { // There's no message for this received, 
+
+	}
+	message_wrapper* to_del = queues_head[receiver_id];
+	msg = queues_head[receiver_id]->msg;
+	queues_head[receiver_id] = queues_head[receiver_id]->next;
+	m_queue[receiver_id].unlock();
+	delete to_del;
+
+
+
+	int ret = 0;
+	return ret;
+}
---
+++ main.cpp
@@ -0,0 +1,209 @@
+/*
+
+
+Compiler: 
+
+Assumptions: 
+- Data length valid range: [0-MAX_DATA_LENTH] inclusive. I.e. 0 length messages are legal.
+- Any thread is allowed to read a message from the 
+- This library is to be used on a device with maximum 256 HW concurent threads 
+
+Test edge cases, 
+producers continue to generate messages beyong legal limits
+
+
+*/
+
+
+#include <iostream>
+
+#include <thread>
+#include <mutex> 
+
+#include <algorithm>
+#include <vector>
+
+#include "BasicMessagePassing.h"
+
+#define MSGS_PER_PRODUCER 100
+
+
+std::mutex m_cout;  // TODO, dont use global object?
+
+/*
+
+To test the BasicMessagePassing new_message, send, and receive, a producer thread will write a message for each other thread,
+/// == dont send to self for now .. and listen for messages destined to it (it will send itsenf a messge too)
+
+*/
+void ProducerTh(uint8_t thread_id,  BasicMessagePassing& bmp, unsigned int max_thread_count) {
+    std::lock_guard<std::mutex> lock(m_cout);
+    std::cout << "[+] Producer thread " << (unsigned int)thread_id << " created. Thread ID: " << std::this_thread::get_id() << std::endl;
+    //message_t *msg = bmp.new_message();
+    //message_t *msg2 = bmp.new_message();
+    //message_t *msg3 = bmp.new_message();
+
+    //// write something in the new message
+    //msg->len = (int)rand() % (MAX_DATA_LENTH+1); // legal length is 0 - MAX_DATA_LENTH. 
+    //for (int i = 0; i < msg->len; i++) {
+    //    msg->data[i] = 0x13;		// init the data with repeated value
+    //}
+    //msg2->len = (int)rand() % (MAX_DATA_LENTH + 1); // legal length is 0 - MAX_DATA_LENTH. 
+    //for (int i = 0; i < msg2->len; i++) {
+    //    msg2->data[i] = 0x13;		// init the data with repeated value
+    //}
+    //msg3->len = (int)rand() % (MAX_DATA_LENTH + 1); // legal length is 0 - MAX_DATA_LENTH. 
+    //for (int i = 0; i < msg3->len; i++) {
+    //    msg3->data[i] = 0x13;		// init the data with repeated value
+    //}
+    //
+    //bmp.send(1, msg);
+    //bmp.send(2, msg2);
+    //bmp.send(3, msg3);
+
+
+  /*  for (unsigned int i = 0; i < max_thread_count; i++) {
+        if (i == thread_id) continue; // don't send to self -- for now at least
+        bmp.send(i, bmp.new_message());
+    }
+  */
+
+}
+
+
+void ConsumerTh(uint8_t thread_id, BasicMessagePassing& bmp) {
+    std::lock_guard<std::mutex> lock(m_cout);
+    std::cout << "[-] Consumer thread " << (unsigned int) thread_id << " created. Thread ID: " << std::this_thread::get_id() << std::endl;
+
+}
+
+void MonitorTh(uint8_t thread_id, BasicMessagePassing& bmp) {
+    std::lock_guard<std::mutex> lock(m_cout);
+    std::cout << "[o] Monitor thread " << (unsigned int)thread_id << " created. Thread ID: " << std::this_thread::get_id() << std::endl;
+}
+
+void ProducerTest1Th(BasicMessagePassing& bmp) {
+    message_t* msg1 = bmp.new_message();
+    message_t* msg2 = bmp.new_message();
+    message_t* msg3 = bmp.new_message();
+
+    // write something in the new message
+    msg1->len = (int)rand() % (MAX_DATA_LENTH + 1); // legal length is 0 - MAX_DATA_LENTH. 
+    for (int i = 0; i < msg1->len; i++) {
+        msg1->data[i] = 0x13;		// init the data with repeated value
+    }
+    msg2->len = (int)rand() % (MAX_DATA_LENTH + 1); // legal length is 0 - MAX_DATA_LENTH. 
+    for (int i = 0; i < msg2->len; i++) {
+        msg2->data[i] = 0x13;		// init the data with repeated value
+    }
+    msg3->len = (int)rand() % (MAX_DATA_LENTH + 1); // legal length is 0 - MAX_DATA_LENTH. 
+    for (int i = 0; i < msg3->len; i++) {
+        msg3->data[i] = 0x13;		// init the data with repeated value
+    }
+
+    bmp.send(1, msg1);
+    bmp.send(2, msg2);
+    bmp.send(3, msg3);
+    bmp.delete_message(msg1);
+    bmp.delete_message(msg2);
+    bmp.delete_message(msg3);
+}
+
+
+void BadUserThread(BasicMessagePassing& bmp) {
+    bmp.delete_message(NULL);
+    //bmp.delete_message(); // TODO -- add malicious addresses? 
+    bmp.send(-1, NULL);
+    bmp.send(2542, NULL);
+    bmp.send(2, NULL);
+    bmp.recv(-2, NULL);
+    bmp.recv(0, NULL);
+    bmp.recv(9876, NULL);
+}
+
+int main()
+{
+    BasicMessagePassing basic_message_passing_uut;
+    std::cout << "Testing Basic Message Passing Library.\n";
+
+    // Test with thread_id 0 creating multiple messages and using send and receive for basic functional testing
+    std::cout << "Test 1: Thread1 creates several messages and sends them around\n";
+    std::thread producer_thread(ProducerTest1Th, std::ref(basic_message_passing_uut)); // thread id is 0 but it doesn't really need it for this test
+    std::thread consumer_thread1(ConsumerTh, 1, std::ref(basic_message_passing_uut));
+    std::thread consumer_thread2(ConsumerTh, 2, std::ref(basic_message_passing_uut));
+
+
+    producer_thread.join();
+    consumer_thread1.join();
+    consumer_thread2.join();
+
+
+
+    /*
+    Test with 10 producer threads sending data to 8 consumer threads
+    */
+
+    std::thread producers[10];
+    std::thread consumers[8];
+    unsigned int created_thread_count = 0;
+
+
+
+    for (auto p : producers) {
+        p = std::thread(ProducerTh, created_thread_count++), std::ref(basic_message_passing_uut));
+    }
+
+
+
+    // Test with at least 3 threads, up to the maximum the hardware can support (dev & testing with 16 thread supper) or 256
+    /*unsigned int*/ created_thread_count = 0;
+    std::cout << "Test 2: Testing with maximum hardware utilization\n";
+
+    unsigned int max_thread_count = std::min((unsigned int)MAX_THREADS_POSSIBLE, std::max((unsigned int)3, std::thread::hardware_concurrency()));
+    std::vector<std::thread> thread_pool(max_thread_count);
+    srand((unsigned)time(NULL));
+
+    std::cout << "Hardware concurrency supports " << max_thread_count << " threads\n";
+    std::cout << "Initializingn " << max_thread_count << " threads to test the BasicMessagePassing library\n";
+
+    // initializing at least 3 threads (1 writer and 2 consumers)
+    // for threads 4-hardware_concurrency randomly choose a thread type
+
+    thread_pool.at(created_thread_count) = std::thread(ProducerTh, created_thread_count, std::ref(basic_message_passing_uut), max_thread_count);
+    created_thread_count++;
+
+    thread_pool.at(created_thread_count) = std::thread(ConsumerTh, created_thread_count, std::ref(basic_message_passing_uut));
+    created_thread_count++;
+
+    thread_pool.at(created_thread_count) = std::thread(MonitorTh, created_thread_count, std::ref(basic_message_passing_uut));
+    created_thread_count++;
+
+    for (; created_thread_count < max_thread_count; created_thread_count++) {
+        thread_pool.at(created_thread_count) = std::thread(ConsumerTh, created_thread_count, std::ref(basic_message_passing_uut)); // TODO: randomize th function
+    }
+
+
+    for (int i = 0; i < created_thread_count; i++) {
+        thread_pool.at(i).join();
+    }
+    /*
+    */
+
+
+
+
+
+
+    /*
+    Tset with a bad user thread that will use invalid inputs to test library input validation
+    */
+
+    /*
+    std::cout << "Test 3: Bad user thread will attempt to use invalid library API input values\n";
+    std::thread bad_thread(BadUserThread, std::ref(basic_message_passing_uut));
+    bad_thread.join();
+    */
+
+    std::cout << "Test program completed execution." << std::endl;
+    return 0;
+}
