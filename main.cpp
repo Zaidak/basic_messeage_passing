11b8f101030406ba9e8bce6abed075c67eae9b3d
Zaid <nolosian@gmail.com>
2022-11-15 20:55:47 -0500

    submitted to kepler + removed include vector

--- main.cpp
+++ main.cpp
@@ -17,6 +17,11 @@ IDE & Compiler used to develop the library and test application:
         /Zc:forScope /RTC1 /Gd /MDd /std:c++20 /FC /Fa"x64\Debug\" /EHsc /nologo /Fo"x64\Debug\" 
         /Fp"x64\Debug\kepler_embedded_sw_challenge_1.pch" /diagnostics:column
 
+
+Design approach: 
+- Given the assumptions below, a singly linked list datastructure is used to build simple fifo queues for each possible thread.
+- This gives very efficient receive function implementation, but adds complexity to the delete_message
+
 Assumptions: 
 - Messages are expected to be received in FIFO order
 - Data length valid range: [0-MAX_DATA_LENTH] inclusive. I.e. 0 length messages are legal.
@@ -32,12 +37,13 @@ Assumptions:
 - The library is assumed designed for an embedded system with limited number of hardware_concurrency support and a known upper limit imposoed by CPU architecture 
     (common Intel X86-64 CPUs are less than 16) 
 - The test app is to fail and throw an exception when a test case fails to produce the expected output; use assert statements
+- Race conditions in the test app implementation are acceptable, its goal is to test the MessagePassingLibrary
 Possible improvements: 
     - Use a thread pool for the test application instead of creating and deleting test threads.
     - Syncrhonize access to std::cout using std::basic_osyncstream in the test app, instead of the global mutex & lock guards
     - Use Joinable threads in the test app to avoid
     - Lock guard scopes in the library likely posssible to be optimized, made smaller, seperate mutexes for a queue pointers can be used - espicially the delete_message function
-
+    - Correct bug in semaphore signaling between producer and consumer threads in the test app
 Notes:
     - Test app uses asserts to verify the library is executing as expected. Error prompts in the console output are expected.
     - When test cases expected values fail, an assert function call will interrupt the test run
@@ -47,7 +53,6 @@ Notes:
 #include <thread>
 #include <mutex> 
 #include <semaphore>
-#include <vector>
 #include <cassert>
 
 #include "BasicMessagePassing.h"
@@ -58,6 +63,8 @@ std::mutex m_cout;
 std::counting_semaphore sem_th0_count_packets{ 0 };                                            // to count packets queued for thread_id 0
 std::counting_semaphore sem_th1_count_packets{ 0 };                                            // to count packets queued for thread_id 1
 
+std::atomic_flag cont_receiving;
+
 // Test 1: simple test of library functions using 3 threads
 void Test1ProducerTh(BasicMessagePassing* bmp); // creates messgae objects, sends to 2 Consumer threads. Doesn't need to receive messages; doesn't need an ID
 void Test1ConsumerTh0(BasicMessagePassing* bmp); // waits for available messages passed to it, deletes received messages after using them
@@ -67,7 +74,7 @@ void Test1ConsumerTh1(BasicMessagePassing* bmp); // waits for available messages
 void BadUserThread(BasicMessagePassing* bmp);
 
 // Test 3: Performance & thread safety - using semaphores, continously sending and receiving messages
-void ProducerTh(BasicMessagePassing* bmp);
+void Test3ProducerTh(BasicMessagePassing* bmp);
 
 
 int main(){
@@ -76,37 +83,42 @@ int main(){
     std::cout << "Testing Basic Message Passing Library." << std::endl;
     std::cout << "Hardware concurrency supports " << std::thread::hardware_concurrency() << " threads\n";
 
-    std::cout << "Test 1: a producer thread creates fixed nmber of messages and sends them in a deterministic way to 2 consumer threads" << std::endl;
+    std::cout << "Test group 1: a producer thread creates fixed nmber of messages and sends them in a deterministic way to 2 consumer threads" << std::endl;
     p_basic_message_passing_uut = new BasicMessagePassing();
+    cont_receiving.test_and_set(); // enable consumers, will be desabled at the end of Test1ProducerTh
     std::thread producer_thread(Test1ProducerTh, p_basic_message_passing_uut);
-    std::thread consumer_thread1(Test1ConsumerTh0, p_basic_message_passing_uut);
-    std::thread consumer_thread2(Test1ConsumerTh1, p_basic_message_passing_uut);
+    std::thread consumer_thread0(Test1ConsumerTh0, p_basic_message_passing_uut);
+    std::thread consumer_thread1(Test1ConsumerTh1, p_basic_message_passing_uut);
     producer_thread.join();
-    consumer_thread1.detach();
-    consumer_thread2.detach();
+    consumer_thread0.join();
+    consumer_thread1.join();
     delete p_basic_message_passing_uut;
 
 
-    std::cout << "Test 2: Bad user thread will attempt to use invalid library API input values" << std::endl;
+    std::cout << "Test group 2: Bad user thread will attempt to use invalid library API input values" << std::endl;
     p_basic_message_passing_uut = new BasicMessagePassing();
     std::thread bad_thread(BadUserThread, std::ref(p_basic_message_passing_uut));
     bad_thread.join();
     delete p_basic_message_passing_uut;
 
-    std::cout << "Test 3: 3 producer threads continuously sending messages to the 2 consumer threads from test 1" << std::endl ;
+
+    std::cout << "Test group 3: 3 producer threads continuously sending messages to the 2 consumer threads from test 1" << std::endl ;
     p_basic_message_passing_uut = new BasicMessagePassing();
-    std::thread producers[3];
-
-
+    std::thread cont_producers[3];
     for(int i=0; i<3 ;i++){
-        producers[i] = std::thread(ProducerTh, p_basic_message_passing_uut);
+        cont_producers[i] = std::thread(Test3ProducerTh, p_basic_message_passing_uut);
     }
-
+    std::thread cont_consumer0(Test1ConsumerTh0, p_basic_message_passing_uut);
+    std::thread cont_consumer1(Test1ConsumerTh1, p_basic_message_passing_uut);
     for (int i = 0; i < 3; i++) {
-        producers[i].join();
+        cont_producers[i].join();
     }
+    cont_consumer0.join();
+    cont_consumer1.join();
+
 
     std::cout << "Test program completed execution sucessfully." << std::endl;
+    std::cout << "Unless a failed assertion is encountered, the test results matched expectations." << std::endl;
     return 0;
 }
 
@@ -166,7 +178,7 @@ void Test1ProducerTh(BasicMessagePassing* bmp) {
     assert(status == BasicMessagePassing::SUCCESS);
     {
         std::lock_guard<std::mutex> lg_cout(m_cout);
-        std::cout << " Producer thread sent msg 2 to thread two times, 3 sends of msg 2 are now in queue." << std::endl;
+        std::cout << " Producer thread sent msg 2 to thread 2 times, 3 sends of msg 2 are now in queue." << std::endl;
     }
 
     bmp->delete_message(msg2);
@@ -183,8 +195,9 @@ void Test1ProducerTh(BasicMessagePassing* bmp) {
         std::cout << " Producer thread attempted to read a msg in the queue to thread 2 (which should be empty now)." << std::endl;
         std::cout << " Producer thread will sleep for 5 seconds, then delete the remaining messages and exit" << std::endl;
     }
+    cont_receiving.clear();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(4000)); // To provide a break in the console output for manual testing
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // To provide a break in the console output for manual testing
     bmp->delete_message(msg0);
     bmp->delete_message(msg1);
 }
@@ -196,21 +209,15 @@ void Test1ConsumerTh0(BasicMessagePassing* bmp) {
     }
     message_t* msg_recevied = NULL;
     int status;
-    while (1) {
+    while (1){
+        if (cont_receiving.test()) break;
         sem_th0_count_packets.acquire();
         status = bmp->recv(0, msg_recevied);
-        //{
-            //if(status == BasicMessagePassing::SUCCESS)
-            //{
-                assert(msg_recevied != NULL);
-                std::lock_guard<std::mutex> lg_cout(m_cout);
-                std::cout << "  Consumer thread 0 received msg of length: " << (unsigned int)msg_recevied->len << std::endl;
-            //}
-            //else {
-            //    std::lock_guard<std::mutex> lg_cout(m_cout);
-            //    std::cout << "ERR  Consumer thread 0 encountered an UNEXPECETD error trying to receive a message." << std::endl;
-            //}
-        //}
+        if (status == BasicMessagePassing::SUCCESS) {
+            assert(msg_recevied != NULL);
+            std::lock_guard<std::mutex> lg_cout(m_cout);
+            std::cout << "  Consumer thread 0 received msg of length: " << (unsigned int)msg_recevied->len << std::endl;
+        }
     }
 }
 void Test1ConsumerTh1(BasicMessagePassing* bmp) {
@@ -220,12 +227,15 @@ void Test1ConsumerTh1(BasicMessagePassing* bmp) {
     }
     message_t* msg_recevied = NULL;
     int status;
-    while (1) {
+    while (1){
+        if (cont_receiving.test()) break;
         sem_th1_count_packets.acquire();
         status = bmp->recv(1, msg_recevied);
-                assert(msg_recevied != NULL);
-                std::lock_guard<std::mutex> lg_cout(m_cout);
-                std::cout << "  Consumer thread 1 received msg of length: " << (unsigned int)msg_recevied->len << std::endl;
+        if (status == BasicMessagePassing::SUCCESS) {
+            assert(msg_recevied != NULL);
+            std::lock_guard<std::mutex> lg_cout(m_cout);
+            std::cout << "  Consumer thread 1 received msg of length: " << (unsigned int)msg_recevied->len << std::endl;
+        }
     }
 }
 
@@ -262,13 +272,33 @@ void BadUserThread(BasicMessagePassing* bmp) {
     std::cout << "              - recv(0, msg) - should return THREAD_QUEUE_EMPTY" << std::endl;
     status = bmp->recv(0, msg);
     assert(status == BasicMessagePassing::THREAD_QUEUE_EMPTY);
-    
+    std::cout << "              - Sleep for 5 seconds before next test stage" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000)); // To provide a break in the console output for manual testing
+
     return;
 }
-
-void ProducerTh( BasicMessagePassing* bmp){
+#define SEND_BURST_COUNT 1000
+void Test3ProducerTh(BasicMessagePassing* bmp){
     {
         std::lock_guard<std::mutex> lg_cout(m_cout);
-        std::cout << "[+] Producer created. Thread ID: " << std::this_thread::get_id() << std::endl;
+        std::cout << "[+] Producer created. Will continuously send messages to 2 consumer threads." << std::endl;
     }
+
+    message_t* msg0 = bmp->new_message();
+    message_t* msg1 = bmp->new_message();
+    msg0->len = 1;
+    msg1->len = 5;
+    for(int i=0;i<SEND_BURST_COUNT; i++){
+        bmp->send(0, msg0);
+        sem_th0_count_packets.release();
+        bmp->send(1, msg0);
+        sem_th1_count_packets.release();
+        bmp->send(0, msg1);
+        sem_th0_count_packets.release();
+        bmp->send(1, msg1);
+        sem_th1_count_packets.release();
+    }
+    cont_receiving.clear();
+
 }
